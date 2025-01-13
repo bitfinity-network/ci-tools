14b08f1b47c1ceaacb0815492f64d8d7afc46cd5
veeso <christian.visintin@veeso.dev>
2025-01-13 17:57:07 +0100

    EPROD-1110 removed https endpoints for testing

--- testing/hirosystems-ordinals-api/ordinals-api.sh
+++ testing/hirosystems-ordinals-api/ordinals-api.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-local-ssl-proxy --source 8005 --target 3000 --key localhost+3-key.pem --cert localhost+3.crt &
-
 cd /app/ordinals-api-regtest/
 # postgres params
 export PGHOST='postgres'
